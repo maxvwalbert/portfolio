f83cadffc3eaec9603d938bee4514859fdf50707
Max Albert <maxalbert@Maxs-MacBook-Pro.local>
2020-04-25 00:13:05 -0400

    setup base site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import { IoLogoGithub } from "react-icons/io";
+import { FaMedium } from "react-icons/fa";
+
 import './App.css';
+
+const portfolioData = [
+  {picture: "hearthstone.jpg", link: "https://github.com/maxvwalbert/hearthScrape", titleText: "eSports Web Scraper", bodyText: "Developed a script that scraped websites to gather data on eSports oppnenents. Used by Michigan Varsity Athletics program for eSport tournaments. This is a python script that uses beautiful soup framework for scraping."},
+  {picture: "bueller.svg", link: "https://github.com/bfreiband/bueller2", titleText: "Bueller", bodyText: "Full stack web application to provide data analytics to professors. Allows students to provide real-time analytics to professors to improve their lecture. Technologies: HTML, CSS, Javascript, and Python. Worked on a colabrative team of two developers"},
+];
+
+function Left() {
+  return (
+    <div id="column1">
+      <h1 id="text01">Max Albert</h1>
+      <p id="text02">Software Developer</p>
+      <p id="text03">I develop full-stack applications in RX-Java, React, and Swift. I also write blogs on agile development principals. <i>Contact me at maxvwalbert@gmail.com</i></p>
+      <ul id="icons01">
+        <li><a href="https://github.com/maxvwalbert"><IoLogoGithub /></a></li>
+        <li><a href="https://medium.com/@maxvwalbert"><FaMedium /></a></li>
+      </ul>
+    </div>
+  );
+}
+
+function Right(props) {
+  return (
+    <a href={props.link}>
+      <div id="project">
+        <div id="imageContainer"><img src={props.picture}></img></div>
+        <div id="text">
+          <h1>{props.titleText}</h1>
+          <p>{props.bodyText}</p>
+        </div>
+    </div>
+    </a>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <div id="Text">
+        <Left />
+      </div>
+      <div id="Projects">
+        {portfolioData.map(project => <Right {...project}/>)}
+      </div>
     </div>
   );
 }
